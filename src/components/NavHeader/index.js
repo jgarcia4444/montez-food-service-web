44fbf366cf1d0d02a02ac4a02013d8bad92a1a55
Jake Garcia <bballwiz4@gmail.com>
2022-06-27 22:02:40 -0700

    The nav bar now shows which page is active by showing a small bottom border under the active link.

--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import '../../styles/nav/NavHeader.css';
 import { Link } from 'react-router-dom';
 import { FiUser } from 'react-icons/fi'
 
-const NavHeader = () => {
+const NavHeader = (props) => {
+
+
+    const [activePage, setActivePage] = useState('');
 
     return (
         <div className="nav-header-row">
             <div className="nav-title-container">
-                <h2 className="nav-title">Montez Food Service</h2>
+                <h2 className="nav-title"><Link onClick={() => setActivePage("Home")} to="/" className="nav-title-link">Montez Food Service</Link></h2>
             </div>
             <div className="nav-links-container">
-                <Link className="nav-link" to="/">Home</Link>
-                <Link className="nav-link" to="/products">Products</Link>
-                <Link className="nav-link" to="/order-online">Order Online</Link>
-                <Link className="nav-link" to="/cost-optimization">Cost Optimization</Link>
+                <Link onClick={() => setActivePage("Home")} className={`nav-link ${activePage === "Home" ? "active-link" : ""}`}to="/">Home</Link>
+                <Link onClick={() => setActivePage("Products")} className={`nav-link ${activePage === "Products" ? "active-link" : ""}`} to="/products">Products</Link>
+                <Link onClick={() => setActivePage("Order Online")} className={`nav-link ${activePage === "Order Online" ? "active-link" : ""}`} to="/order-online">Order Online</Link>
+                <Link onClick={() => setActivePage("Cost Optimization")} className={`nav-link ${activePage === 'Cost Optimization' ? "active-link" : ""}`} to="/cost-optimization">Cost Optimization</Link>
                 <span className="vertical-separator">|</span>
                 <FiUser size={20} color={'black'} />
             </div>
