c55fb4840eb57c990031c4851b186deaddb983da
Jake Garcia <bballwiz4@gmail.com>
2022-06-27 22:14:48 -0700

    Started to add to the content of the home page.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -8,7 +8,20 @@ const Home = () => {
 
     return (
         <Layout>
-            <h1>Home Page</h1>
+            <div className='flex-container'>
+                <div className='half-flex-container'>
+
+                </div>
+                <div className='half-flex-container'>
+                    <div className="content-container">
+                        <h3>YOUR LOCAL FOOD AND RESTAURANT SUPPLIES PROVIDER</h3>
+                        <p>Family Owned, Family Operated, Family Service</p>
+                        <div className="contact-us-button">
+                            <a className="contac-us-anchor" href="tel:9514037288">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
