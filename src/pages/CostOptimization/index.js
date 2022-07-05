31c75d9ced31466206dea13bc3d22214c8eaa22b
Jake Garcia <bballwiz4@gmail.com>
2022-07-05 11:19:48 -0700

    Started to layout the cost optimization page.

--- src/pages/CostOptimization/index.js
+++ src/pages/CostOptimization/index.js
@@ -6,7 +6,28 @@ const CostOptimization = () => {
 
     return (
         <Layout>
-            <h1>Cost Optimization Page</h1>
+            <div className="top-container">
+                <div className="half-column">
+                    <div className="optimization-details">
+                        <div className="optimization-title-row">
+                            <h2 className="optimization-title">Cost Optimization Analysis</h2>
+                        </div>
+                        <div className="optimization-details-container">
+                            <h4 className="optimization-details-title">Are You Currently Paying Too Much?</h4>
+                            <p>We will identify if you are being charged more than you should be, and recommend what we can do to help lower those hefty expenses.</p>
+                        </div>
+                        <div className="optimization-details-container">
+                            <h4 className="optimization-details-title">How Much Can You Save?</h4>
+                            <p>On average, we tend to save customers 12%-15% on food costs and sometimes even more than 20%! This may equate to $1,000 (or more) a week, or $52,000 a year, or a couple of new cars, or even a down payment for a house!!!</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="half-column">
+                    <div className="optimization-details">
+                        <h2>Data Table</h2>
+                    </div>
+                </div>
+            </div>
         </Layout>
     )
 }
