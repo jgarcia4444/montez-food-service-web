11325d6458bd65cbc7d5baf41bb47b346c02617e
Jake Garcia <bballwiz4@gmail.com>
2022-07-06 15:11:55 -0700

    Added in the custom hook to get the screens width and set it up in the nav bar component to handle showing the mobile navigation button once the screen reaches a certain size.

---
+++ src/components/NavHeader/MobileNavButton/index.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import '../../../styles/nav/MobileNavButton.css'
+
+const MobileNavButton = () => {
+
+    return (
+        <div>
+            <h4>Mobile</h4>
+        </div>
+    )
+}
+
+export default MobileNavButton;
--- src/components/NavHeader/index.js
+++ src/components/NavHeader/index.js
@@ -3,12 +3,16 @@ import '../../styles/nav/NavHeader.css';
 import { Link } from 'react-router-dom';
 import { FiUser } from 'react-icons/fi'
 import { connect } from 'react-redux';
+import useWindowDimensions from '../../customHooks/useWindowDimensions';
 
 import { useNavigate } from 'react-router-dom';
+import MobileNavButton from './MobileNavButton';
 
 const NavHeader = ({userInfo}) => {
 
-    const {email} = userInfo
+    const {email} = userInfo;
+
+    const {width} = useWindowDimensions();
 
     const [activePage, setActivePage] = useState('');
     const navigate = useNavigate();
@@ -21,18 +25,32 @@ const NavHeader = ({userInfo}) => {
         }
     }
 
+    const navLinks = (
+        <>
+            <Link onClick={() => setActivePage("Home")} className={`nav-link ${activePage === "Home" ? "active-link" : ""}`}to="/">Home</Link>
+            <Link onClick={() => setActivePage("Products")} className={`nav-link ${activePage === "Products" ? "active-link" : ""}`} to="/products">Products</Link>
+            <Link onClick={() => setActivePage("Order Online")} className={`nav-link ${activePage === "Order Online" ? "active-link" : ""}`} to="/order-online">Order Online</Link>
+            <Link onClick={() => setActivePage("Cost Optimization")} className={`nav-link ${activePage === 'Cost Optimization' ? "active-link" : ""}`} to="/cost-optimization">Cost Optimization</Link>
+            <span className="vertical-separator">|</span>
+            <FiUser onClick={handleUserPress} className="user-icon" size={20} color={'black'} />
+        </>
+    )
+
+    const renderNavLinks = () => {
+        if (width > 800) {
+            return navLinks;
+        } else {
+            return <MobileNavButton />
+        }
+    }
+
     return (
         <div className="nav-header-row">
             <div className="nav-title-container">
                 <h2 className="nav-title"><Link onClick={() => setActivePage("Home")} to="/" className="nav-title-link">Montez Food Service</Link></h2>
             </div>
             <div className="nav-links-container">
-                <Link onClick={() => setActivePage("Home")} className={`nav-link ${activePage === "Home" ? "active-link" : ""}`}to="/">Home</Link>
-                <Link onClick={() => setActivePage("Products")} className={`nav-link ${activePage === "Products" ? "active-link" : ""}`} to="/products">Products</Link>
-                <Link onClick={() => setActivePage("Order Online")} className={`nav-link ${activePage === "Order Online" ? "active-link" : ""}`} to="/order-online">Order Online</Link>
-                <Link onClick={() => setActivePage("Cost Optimization")} className={`nav-link ${activePage === 'Cost Optimization' ? "active-link" : ""}`} to="/cost-optimization">Cost Optimization</Link>
-                <span className="vertical-separator">|</span>
-                <FiUser onClick={handleUserPress} className="user-icon" size={20} color={'black'} />
+                {renderNavLinks()}
             </div>
         </div>
     )
