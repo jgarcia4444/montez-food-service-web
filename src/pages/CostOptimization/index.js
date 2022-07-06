2d766a6ac7f1a8c865bf260d021094740282d6b8
Jake Garcia <bballwiz4@gmail.com>
2022-07-05 21:15:14 -0700

    Added a couple media break point styles to the home page to collapse columns when the screen size becomes too small.

--- src/pages/CostOptimization/index.js
+++ src/pages/CostOptimization/index.js
@@ -12,13 +12,15 @@ const CostOptimization = () => {
                         <div className="optimization-title-row">
                             <h2 className="optimization-title">Cost Optimization Analysis</h2>
                         </div>
-                        <div className="optimization-details-container">
-                            <h4 className="optimization-details-title">Are You Currently Paying Too Much?</h4>
-                            <p>We will identify if you are being charged more than you should be, and recommend what we can do to help lower those hefty expenses.</p>
-                        </div>
-                        <div className="optimization-details-container">
-                            <h4 className="optimization-details-title">How Much Can You Save?</h4>
-                            <p>On average, we tend to save customers 12%-15% on food costs and sometimes even more than 20%! This may equate to $1,000 (or more) a week, or $52,000 a year, or a couple of new cars, or even a down payment for a house!!!</p>
+                        <div className="smaller-container">
+                            <div className="optimization-details-container">
+                                <h4 className="optimization-details-title">Are You Currently Paying Too Much?</h4>
+                                <p>We will identify if you are being charged more than you should be, and recommend what we can do to help lower those hefty expenses.</p>
+                            </div>
+                            <div className="optimization-details-container">
+                                <h4 className="optimization-details-title">How Much Can You Save?</h4>
+                                <p>On average, we tend to save customers 12%-15% on food costs and sometimes even more than 20%! This may equate to $1,000 (or more) a week, or $52,000 a year, or a couple of new cars, or even a down payment for a house!!!</p>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -57,6 +59,61 @@ const CostOptimization = () => {
                                 </div>
                                 <div className="table-quarter-column">
                                     $7,500
+                                </div>
+                            </div>
+                            <div className="table-data-row even-row">
+                                <div className="table-half-column">
+                                    Spices and Grains
+                                </div>
+                                <div className="table-quarter-column middle-column">
+                                    ~$50
+                                </div>
+                                <div className="table-quarter-column">
+                                    $2,500
+                                </div>
+                            </div>
+                            <div className="table-data-row">
+                                <div className="table-half-column">
+                                    Potential for cleaning supplies/services
+                                </div>
+                                <div className="table-quarter-column middle-column">
+                                    ~$100
+                                </div>
+                                <div className="table-quarter-column">
+                                    $5,000
+                                </div>
+                            </div>
+                            <div className="table-data-row even-row">
+                                <div className="table-half-column">
+                                    Assistance in price negotiations for things such as produce
+                                </div>
+                                <div className="table-quarter-column middle-column">
+                                    ~$150
+                                </div>
+                                <div className="table-quarter-column">
+                                    $7,500
+                                </div>
+                            </div>
+                            <div className="table-data-row">
+                                <div className="table-half-column">
+                                    Equipment and supplies for rest/catering
+                                </div>
+                                <div className="table-quarter-column middle-column">
+                                    ?
+                                </div>
+                                <div className="table-quarter-column">
+                                    ?
+                                </div>
+                            </div>
+                            <div className="table-data-row even-row">
+                                <div className="table-half-column">
+                                    <strong>Total</strong>
+                                </div>
+                                <div className="table-quarter-column middle-column">
+                                    <strong>$1,425</strong>
+                                </div>
+                                <div className="table-quarter-column">
+                                    <strong>$72,500</strong>
                                 </div>
                             </div>
                         </div>
