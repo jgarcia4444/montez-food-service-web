4d5b859d05cded20f22d973ec5269a4e1f877ef5
Jake Garcia <bballwiz4@gmail.com>
2022-07-26 15:19:38 -0700

    Added a configure total price funcion as well as a handle item selection function to the order form component.

--- src/components/OrderForm/index.js
+++ src/components/OrderForm/index.js
@@ -10,20 +10,50 @@ const OrderForm = ({userInfo}) => {
 
     const [itemText, setItemText] = useState('');
     const [itemPrice, setItemPrice] = useState('')
+    const [quantity, setQuantity] = useState('1');
+    const [selectedItem, setSelectedItem] = useState({});
 
     const {email, companyName} = userInfo;
+
+    const configureTotalPrice = () => {
+        if (itemPrice === "") {
+            return ""
+        } else {
+            let priceNum = parseFloat(itemPrice);
+            let totalPrice = (parseInt(quantity) * priceNum).toFixed(2);
+            return totalPrice;
+        }
+    }
+
+    const handleItemSelection = (itemInfo) => {
+        setSelectedItem(itemInfo);
+        setItemPrice(itemInfo.price);
+    }
+
+    const addItemToOrder = () => {
+
+    }
 
     return (
         <div className="order-form-container">
             <div className="order-form-row">
-                <ItemFinder setItemText={setItemText} itemText={itemText} suggestions={[]} />
+                <ItemFinder selectedItem={selectedItem} setSelectedItem={handleItemSelection} setItemText={setItemText} itemText={itemText} suggestions={[]} />
                 <div className="item-price-container">
                     <h3>Price</h3>
                     {itemPrice === "" ? "" : `$${itemPrice}`}
                 </div>
             </div>
             <div className="order-form-row">
-                <QuantitySelector />
+                <QuantitySelector quantity={quantity} setQuantity={setQuantity} />
+                <div className="total-price-container">
+                    <h3>Total Price</h3>
+                    <p><strong>{configureTotalPrice()}</strong></p>
+                </div>
+            </div>
+            <div className="order-form-row">
+                <div onClick={addItemToOrder} className="add-to-order-button">
+                    Add To Order
+                </div>
             </div>
         </div>
     )
