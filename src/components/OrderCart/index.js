b0fe7bf17c263c5c2ff68921d634a68de2c5e20a
Jake Garcia <bballwiz4@gmail.com>
2022-08-02 10:52:32 -0700

    Added in a total calculations segment to the order cart and created three functios to calulate the subtotal, the estimated taxes and the calculated total.

--- src/components/OrderCart/index.js
+++ src/components/OrderCart/index.js
@@ -15,11 +15,53 @@ const OrderCart = ({items}) => {
         }
     }
 
+    const calculateSubTotal = () => {
+        if (items.length === 0) {
+            return "0.00";
+        } else {
+            var subTotal = 0.00;
+            items.forEach(item => subTotal += parseFloat(item.price))
+            return subTotal.toFixed(2);
+        }
+    }
+
+    const calculateSalesTax = () => {
+        if (items.length === 0) {
+            return "0.00";
+        } else {
+            var taxesTotal = 0.00;
+            items.forEach(item => taxesTotal += (parseFloat(item.price) * 0.0725))
+            return taxesTotal.toFixed(2);
+        }
+    }
+
+    const calculateOrderTotal = () => {
+        if (items.length === 0) {
+            return "0.00";
+        } else {
+            return (parseFloat(calculateSubTotal()) + parseFloat(calculateSalesTax())).toFixed(2);
+        }
+    }
+
     return (
         <div className="order-cart-container">
             <h2 className="order-cart-title">Order</h2>
             <div className="order-items-container">
                 {renderOrderItems()}
+            </div>
+            <div className="calculated-total-container">
+                <div className="sub-total-row">
+                    <h4>Subtotal</h4>
+                    ${calculateSubTotal()}
+                </div>
+                <div className="calculated-taxes-row">
+                    <h4>Taxes</h4>
+                    ${calculateSalesTax()}
+                </div>
+                <div className="total-row">
+                    <h4>Total</h4>
+                    ${calculateOrderTotal()}
+                </div>
             </div>
         </div>
     )
