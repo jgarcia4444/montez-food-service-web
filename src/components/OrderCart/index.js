e0a13dab0eb05b39ab5b2e91a89226819480ff3e
Jake Garcia <bballwiz4@gmail.com>
2022-08-01 17:36:28 -0700

    Changed the style of the add to order button so that it is left aligned.

---
+++ src/components/OrderCart/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const OrderCart = () => {
+
+    return (
+        <div className="order-cart-container">
+            <h2>Order</h2>
+        </div>
+    )
+}
+
+export default OrderCart;
--- src/pages/OrderOnline/index.js
+++ src/pages/OrderOnline/index.js
@@ -2,13 +2,21 @@ import React from 'react';
 import '../../styles/orderOnline/OrderOnline.css';
 import Layout from '../../shared/Layout';
 import OrderForm from '../../components/OrderForm';
+import OrderCart from '../../components/OrderCart';
 
 const OrderOnline = () => {
 
     return (
         <Layout>
             <h1 className="page-title">Order Online</h1>
-            <OrderForm />
+            <div className="order-online-container">
+                <div className="seventy-width-container">
+                    <OrderForm />
+                </div>
+                <div className="thirty-width-container">
+                    <OrderCart />
+                </div>
+            </div>
         </Layout>
     )
 }
