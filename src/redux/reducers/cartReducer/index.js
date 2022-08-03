185c5756761c8252a36d717da889a43e3e73bf52
Jake Garcia <bballwiz4@gmail.com>
2022-08-02 21:13:21 -0700

    When a user adds the same item to the order cart it simply updates the quantity of the order item.

--- src/redux/reducers/cartReducer/index.js
+++ src/redux/reducers/cartReducer/index.js
@@ -6,9 +6,30 @@ const initialState = {
 const cartReducer = (state=initialState, action) => {
     switch(action.type) {
         case "ADD_ITEM_TO_CART":
-            return {
-                ...state,
-                items: state.items.concat(action.cartItem),
+            if (state.items.some(item => item.description === action.cartItem.description)) {
+                let itemToBeUpdatedIndex = 0;
+                for (let i = 0; i < state.items.length; i++) {
+                    let iItem = state.items[i];
+                    if (iItem.description === action.cartItem.description) {
+                        itemToBeUpdatedIndex = i
+                        break;
+                    }
+                }
+                let itemToBeUpdated = state.items[itemToBeUpdatedIndex];
+                let leftItems = itemToBeUpdatedIndex !== 0 ? state.items.slice(0, itemToBeUpdatedIndex) : [];
+                let rightItems = itemToBeUpdatedIndex !== state.items.length - 1 ? state.items.slice(itemToBeUpdatedIndex + 1) : [];
+                itemToBeUpdated.quantity = parseInt(itemToBeUpdated.quantity) + parseInt(action.cartItem.quantity);
+                itemToBeUpdated.totalPrice = (parseFloat(itemToBeUpdated.price) * parseInt(itemToBeUpdated.quantity)).toFixed(2);
+                let updatedItems = [...leftItems, ...rightItems, itemToBeUpdated];
+                return {
+                    ...state,
+                    items: updatedItems,
+                };
+            } else {
+                return {
+                    ...state,
+                    items: state.items.concat(action.cartItem),
+                }
             }
         default:
             return {
