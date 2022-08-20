dc1674c6bd37a88db6d41467b9347c2f3c5b20f7
Jake Garcia <bballwiz4@gmail.com>
2022-08-19 18:53:02 -0700

    Added the needed logic to the send order redux action that will send information to the backend to save an order and then send a confirmation email.

--- src/redux/actions/cartActions/sendOrder.js
+++ src/redux/actions/cartActions/sendOrder.js
@@ -1,7 +1,40 @@
 import { baseUrl } from '../../../config/Urls'
 
-const sendOrder = () => {
+const sendOrder = (orderDetails) => {
+    const {email, items} = orderDetails;
+    const url = `${baseUrl}order-online/orders`;
+    let configuredBody = {
+        user_info: {
+            email: email
+        },
+        order_info: {
+            items: items
+        }
+    }
+    const options = {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(configuredBody),
+    };
 
+    return async dispatch => {
+        dispatch({type: "SENDING_ORDER"});
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                let {success} = data;
+                if (success === true) {
+                    let {pastOrders} = data;
+                    return dispatch({type: "ORDER_SEND_SUCCESS", pastOrders});
+                } else {
+                    let {error} = data;
+                    let {message} = error;
+                    return dispatch({type: "ORDER_SEND_ERROR", errorMessage: message});
+                }
+            })
+    }
 }
 
 export default sendOrder;
