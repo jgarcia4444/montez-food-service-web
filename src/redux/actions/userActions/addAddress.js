d92c34862b1bfc4a64c1669fa2ac16c8bd04bcba
Jake Garcia <bballwiz4@gmail.com>
2022-12-10 20:19:04 +0530

    Created the addAddress action that will send the information to the backend to add an address to a users account.

--- src/redux/actions/userActions/addAddress.js
+++ src/redux/actions/userActions/addAddress.js
@@ -1,8 +1,47 @@
 import Urls from "../../../config/Urls";
 const {baseUrl} = Urls;
 
-const addAddress = addressInfo => {
-    
+const formatAddress = address => {
+    return {
+        number: address.number,
+        street_name: address.streetName,
+        city: address.city,
+        state: address.state,
+        zip_code: address.zipCode,
+    }
+}
+
+const addAddress = (addressInfo, userId) => {
+
+    const url = `${baseUrl}addresses/${userId}`;
+
+    const formattedAddress = formatAddress(addressInfo)
+
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(formattedAddress)
+    }
+
+    return async dispatch => {
+        dispatch({type: "SAVING_ADDRESS"});
+        fetch(url, options)
+            .then(res => res.json())
+            .then(data => {
+                let {success} = data;
+                if (success === true) {
+                    let {usersAddress} = data;
+                    return dispatch({type: "ADDRESS_SAVE_SUCCESS", usersAddress});
+                } else {
+                    let {error} = data;
+                    let {message} = error;
+                    return dispatch({type: "ADDRESS_SAVE_ERROR", message});
+                }
+            })
+    }
+
 }
 
 export default addAddress;
