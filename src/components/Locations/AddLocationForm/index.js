3d965a82c5e55a941462641b200883b1af3bbb51
Jake Garcia <bballwiz4@gmail.com>
2022-12-15 22:06:54 +0530

    added in the zip code input as well as added in a div to add the save location button.

--- src/components/Locations/AddLocationForm/index.js
+++ src/components/Locations/AddLocationForm/index.js
@@ -14,6 +14,8 @@ const AddLocationForm = ({closeForm}) => {
     const [cityError, setCityError] = useState("");
     const [state, setState] = useState("");
     const [stateError, setStateError] = useState("");
+    const [zipCode, setZipCode] = useState("");
+    const [zipCodeError, setZipCodeError] = useState("");
 
     let streetAddressInputObject = {
         label: "Street",
@@ -29,6 +31,14 @@ const AddLocationForm = ({closeForm}) => {
         icon: <FiHome size={24} color={"#ffc72c"}/>,
         changeFunction: val => setCity(val),
         error: cityError,
+    }
+
+    let zipCodeInputObject = {
+        label: "Zip Code",
+        value: zipCode,
+        changeFunction: val => setZipCode(val),
+        icon: <FiHome size={24} color={"#ffc72c"}/>,
+        error: zipCodeError,
     }
 
     return (
@@ -50,12 +60,18 @@ const AddLocationForm = ({closeForm}) => {
                         <FormInput inputInfo={cityInputObject}/>
                         <div className="state-select-container">
                             <label htmlFor="state" className="state-select-label">State</label>
-                            <select onChange={(val) => setState(val.target.value)} name="state">
+                            <select className="state-select" onChange={(val) => setState(val.target.value)} name="state">
                                 <option value="none" selected disabled hidden>Select an option...</option>
                                 <option value="Ca">Ca</option>
                             </select>
                         </div>
                     </div>
+                    <div className="add-location-form-input-row">
+                        <FormInput inputInfo={zipCodeInputObject} />
+                    </div>
+                </div>
+                <div className="save-location-button">
+                    Add Location
                 </div>
             </div>
         </div>
