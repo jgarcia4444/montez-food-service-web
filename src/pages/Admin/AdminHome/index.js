bed5902f86534ed104c7466b66a5e6e7cf5f4830
Jake Garcia <bballwiz4@gmail.com>
2022-12-26 11:26:26 -0800

    Added in a check that will check if the admins's username is set in the redux state.

--- src/pages/Admin/AdminHome/index.js
+++ src/pages/Admin/AdminHome/index.js
@@ -1,7 +1,18 @@
 import React, {useEffect} from 'react';
+import { connect } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import Layout from '../../../shared/Layout';
 
-const AdminHome = () => {
+const AdminHome = ({username}) => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (username === "") {
+            navigate("/");
+        }
+    })
 
     return (
         <Layout>
@@ -12,4 +23,13 @@ const AdminHome = () => {
     )
 }
 
-export default AdminHome;
+const mapStateToProps = state => {
+    return {
+        username: state.admin.username,
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    null
+)(AdminHome);
