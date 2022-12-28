bbaa0613a762a23528ddb1d565ca8cafaaa330b0
Jake Garcia <bballwiz4@gmail.com>
2022-12-27 17:42:32 -0800

    added the bulk of the logic inside the PendingOrderDetails component.

--- src/pages/Admin/PendingOrderDetails/index.js
+++ src/pages/Admin/PendingOrderDetails/index.js
@@ -4,23 +4,69 @@ import { useParams } from 'react-router-dom';
 
 import fetchOrderDetails from '../../../redux/actions/pendingOrderActions/fetchOrderDetails';
 
+import '../../../styles/Global.css';
+
+import Layout from '../../../shared/Layout';
+
 const PendingOrderDetails = ({pendingOrderDetails, fetchOrderDetails}) => {
     const params = useParams();
     const {orderId} = params;
 
     const {companyName, createdAt, totalPrice, deliveryAddress, items, loadingError} = pendingOrderDetails;
 
+
+    const configuredAddress = () => {
+        if (deliveryAddress !== null) {
+            const {street, city, state, zipCode} = deliveryAddress;
+            return `${street}, ${city}, ${state}, ${zipCode}`;
+        } else {
+            return '';
+        }
+    }
+
     useEffect(() => {
         if (companyName === "" && loadingError === "") {
-            // fetch order details
             fetchOrderDetails(orderId);
         }
     },[companyName])
 
     return (
-        <div className="pending-order-details-container">
-            <h2>Pending Order</h2>
-        </div>
+        <Layout>
+            <div className="pending-order-details-container">
+                <div className="section-title-row">
+                    <h2 className="section-title">Pending Order</h2>
+                </div>
+                <div className="pending-order-details-row">
+                    <div className="order-details-label-container">
+                        <h4 className="order-details-label">Company:</h4>
+                    </div>
+                    <div className="order-details-value-container">
+                        <p className="order-details-value">{companyName}</p>
+                    </div>
+                    <div className="order-details-label-container">
+                        <h4 className="order-details-label">Order Date:</h4>
+                    </div>
+                    <div className="order-details-value-container">
+                        <p className="order-details-value">{createdAt}</p>
+                    </div>
+                </div>
+                <div className="pending-order-details-row">
+                    <div className="order-details-label-container">
+                        <h4 className="order-details-label">Total:</h4>
+                    </div>
+                    <div className="order-details-value-container">
+                        <p className="order-details-value">{totalPrice}</p>
+                    </div>
+                    <div className="order-details-label-container">
+                        <h4 className="order-details-label">Delivery To:</h4>
+                    </div>
+                    <div className="order-details-value-container">
+                        <p className="order-details-value">{configuredAddress()}</p>
+                    </div>
+                </div>
+                <div className="pending-order-details-row"></div>
+            </div>
+        </Layout>
     )
 }
 
