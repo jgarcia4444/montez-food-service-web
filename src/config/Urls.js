ed395bf559529ddd64b730d7a9a721f2f0d22327
Jake Garcia <bballwiz4@gmail.com>
2023-01-08 22:18:02 -0800

    Added in the website title and the favicon

--- src/config/Urls.js
+++ src/config/Urls.js
@@ -1,5 +1,5 @@
 import ReactGA from 'react-ga';
 export default {
-    baseUrl: 'http://127.0.0.1:3000/',
+    baseUrl: 'http://ec2-54-219-183-185.us-west-1.compute.amazonaws.com:3000/',
     reactGa: ReactGA.initialize('G-7380SQJ6M9'),
 }
