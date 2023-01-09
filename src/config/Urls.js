3556fc7511325c491e5ab136b389a33c10b1aef0
Jake Garcia <bballwiz4@gmail.com>
2023-01-08 22:57:18 -0800

    Added in a meta tag to allow http requests.

--- src/config/Urls.js
+++ src/config/Urls.js
@@ -1,5 +1,5 @@
 import ReactGA from 'react-ga';
 export default {
-    baseUrl: 'http://ec2-54-219-183-185.us-west-1.compute.amazonaws.com:3000/',
+    baseUrl: 'https://ec2-54-219-183-185.us-west-1.compute.amazonaws.com:3000/',
     reactGa: ReactGA.initialize('G-7380SQJ6M9'),
 }
