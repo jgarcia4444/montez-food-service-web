353424bc1d77f470bcb505afece61c7b38a8a660
Jake Garcia <bballwiz4@gmail.com>
2023-01-12 15:11:29 -0800

    changed the url that the app points to.

--- src/config/Urls.js
+++ src/config/Urls.js
@@ -1,5 +1,8 @@
 import ReactGA from 'react-ga';
-export default {
-    baseUrl: 'http://ec2-54-219-183-185.us-west-1.compute.amazonaws.com:3000/',
+
+let Urls = {
+    baseUrl: 'https://montez-food-service-api.herokuapp.com/',
     reactGa: ReactGA.initialize('G-7380SQJ6M9'),
 }
+
+export default Urls;
