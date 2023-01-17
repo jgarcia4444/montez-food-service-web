4a8d3e9267a144e807d2a7b6912042611269a574
Jake Garcia <bballwiz4@gmail.com>
2023-01-17 12:02:22 -0800

    Fixed the url to send a get request to the rails backend now.

--- src/redux/actions/oauthActions/getTokens.js
+++ src/redux/actions/oauthActions/getTokens.js
@@ -1,21 +1,22 @@
 
 import Urls from "../../../config/Urls"
-const {tokenUrl} = Urls;
+const {tokenUrl, baseUrl} = Urls;
 
 const getTokens = (authorizationInfo) => {
-    let redirectUri = "https://montez-food-service-web.vercel.app/users/admin"
-    let {authorizationCode, realmID} = authorizationInfo
-    let url = `${tokenUrl}?grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${redirectUri}`;
-    let options = {
-        method: "POST",
-        mode: 'no-cors',
-        headers: {
-            'Accept': 'application/json'
-        }
-    }
+    // let redirectUri = "https://montez-food-service-web.vercel.app/users/admin"
+    let {authorizationCode, realmID} = authorizationInfo;
+    // let url = `${tokenUrl}?grant_type=authorization_code&code=${authorizationCode}&redirect_uri=${redirectUri}`;
+    // let options = {
+    //     method: "POST",
+    //     mode: 'no-cors',
+    //     headers: {
+    //         'Accept': 'application/json'
+    //     }
+    // }
+    let url = `${baseUrl}oauth/tokens/get/${authorizationCode}`;
     return async dispatch => {
         dispatch({type: "FETCHING_TOKENS"});
-        fetch(url, options)
+        fetch(url)
             .then(res => res.json())
             .then(data => {
                 console.log("Data from get tokens action: ", data);
