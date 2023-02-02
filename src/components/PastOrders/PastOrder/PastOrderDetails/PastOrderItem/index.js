47752466772942573e2d2d837a5fb069ba0840e5
Jake Garcia <bballwiz4@gmail.com>
2023-02-01 17:14:02 -0800

    Added the react-redux connect method to the past order item. Render and edit button below the order item if the admin is logged in.

--- src/components/PastOrders/PastOrder/PastOrderDetails/PastOrderItem/index.js
+++ src/components/PastOrders/PastOrder/PastOrderDetails/PastOrderItem/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import {FiEdit2} from 'react-icons/fi';
 
 import '../../../../../styles/components/PastOrders/PastOrder/PastOrderDetails/PastOrderItem/PastOrderItem.css';
 
-const PastOrderItem = ({item}) => {
+const PastOrderItem = ({item, adminUsername}) => {
 
     const {itemInfo, quantity} = item;
     const {description, price, caseBought, caseCost, unitsPerCase } = itemInfo;
@@ -25,29 +27,53 @@ const PastOrderItem = ({item}) => {
     }
 
     return (
-        <div className="past-order-item-container">
-            <div className="item-info-block-row">
-                <div className="item-info-block">
-                    <div className="item-info-label">Name</div>
-                    <div className="item-info-value">{configureDescription()}</div>
+        <>
+            <div className="past-order-item-container">
+                <div className="item-info-block-row">
+                    <div className="item-info-block">
+                        <div className="item-info-label">Name</div>
+                        <div className="item-info-value">{configureDescription()}</div>
+                    </div>
+                    <div className="item-info-block">
+                        <div className="item-info-label">Price Per</div>
+                        <div className="item-info-value">${renderPricePer()}</div>
+                    </div>
                 </div>
-                <div className="item-info-block">
-                    <div className="item-info-label">Price Per</div>
-                    <div className="item-info-value">${renderPricePer()}</div>
+                <div className="item-info-block-row">
+                    <div className="item-info-block">
+                        <div className="item-info-label"># Ordered</div>
+                        <div className="item-info-value">{quantity}</div>
+                    </div>
+                    <div className="item-info-block">
+                        <div className="item-info-label">Total Price</div>
+                        <div className="item-info-value">${calculateTotalPrice()}</div>
+                    </div>
                 </div>
             </div>
-            <div className="item-info-block-row">
-                <div className="item-info-block">
-                    <div className="item-info-label"># Ordered</div>
-                    <div className="item-info-value">{quantity}</div>
+            {adminUsername !== "" &&
+                <div className="order-item-edit-action-row">
+                    <div className="order-item-edit-button">
+                        <FiEdit2 size={20} color={"#fff"} />
+                    </div>
                 </div>
-                <div className="item-info-block">
-                    <div className="item-info-label">Total Price</div>
-                    <div className="item-info-value">${calculateTotalPrice()}</div>
-                </div>
-            </div>
-        </div>
+            }
+        </>
     )
 }
 
-export default PastOrderItem
+const mapStateToProps = state => {
+    return {
+        adminUsername: state.admin.username,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+    )(PastOrderItem);
